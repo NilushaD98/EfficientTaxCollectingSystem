78288e317305bdb753bad9b5a98d57ed6f42bd1d
Nilusha Disanayake <shannilusharr456@gmail.com>
2024-02-05 10:42:39 +0530

    assing a parent server

---
+++ Raven/api-gateway/src/main/java/taxproject/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package taxproject.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Raven/raven-config/src/test/java/com/ravenconfig/RavenConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravenconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavenConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
